8701938dbc71c312e748c7335e768a0f84357cf2
Heidi Sprouse <hsprouse91@gmail.com>
2020-07-10 11:27:55 -0500

    Tag model, repo, and controller have been added

---
+++ Tabloid/Repositories/TagRepository.cs
@@ -0,0 +1,56 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Tabloid.Data;
+using Tabloid.Models;
+using Microsoft.VisualBasic;
+using System.Security.Claims;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+
+namespace Tabloid.Repositories
+{
+
+    public class TagRepository
+    {
+        //saving an instance of our app db context
+        private readonly ApplicationDbContext _context;
+
+        public TagRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Tag> GetAll()
+        {
+            return _context.Tag.ToList();
+        }
+
+        public Tag GetById(int id)
+        {
+            return _context.Tag.FirstOrDefault(p => p.Id == id);
+        }
+
+        public void Add(Tag tag)
+        {
+            _context.Add(tag);
+            _context.SaveChanges();
+        }
+
+        public void Update(Tag tag)
+        {
+            _context.Entry(tag).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var tag = GetById(id);
+            _context.Post.Remove(tag);
+            _context.SaveChanges();
+        }
+
+    }
+}
