459846f1bcf9eec408d734ef54eaf7c6ddd9c974
Heidi Sprouse <hsprouse91@gmail.com>
2020-07-14 10:29:06 -0500

    fixed closing tag issue in app.j

--- Tabloid/client/src/App.js
+++ Tabloid/client/src/App.js
@@ -4,28 +4,28 @@ import { UserProfileProvider } from "./providers/UserProfileProvider";
 import Header from "./components/Header";
 import ApplicationViews from "./components/ApplicationViews";
 import { PostProvider } from './providers/PostProvider';
-import {CategoryProvider} from './providers/CategoryProvider';
+import { CategoryProvider } from './providers/CategoryProvider';
 import { SubscriptionProvider } from './providers/SubscriptionProvider';
 import { TagProvider } from './providers/TagProvider';
 import { PostTagProvider } from './providers/PostTagProvider';
 
+//it is important that PostTagProvider stays outside of Post Provider
 function App() {
   return (
-      <Router>
-          //it is important that PostTagProvider stays outside of Post Provider
-          <UserProfileProvider>
-            <PostTagProvider>
-                <PostProvider>
-                    <CategoryProvider>     
-                        <SubscriptionProvider>       
-                            <TagProvider>
-                                <Header />
-                                <ApplicationViews />              
-                            </TagProvider>
-                        </SubscriptionProvider>
-                    </CategoryProvider>
-                </PostProvider>
-            <PostTagProvider>
+    <Router>
+      <UserProfileProvider>
+        <PostTagProvider>
+          <PostProvider>
+            <CategoryProvider>
+              <SubscriptionProvider>
+                <TagProvider>
+                  <Header />
+                  <ApplicationViews />
+                </TagProvider>
+              </SubscriptionProvider>
+            </CategoryProvider>
+          </PostProvider>
+        </PostTagProvider>
       </UserProfileProvider>
     </Router>
   );
