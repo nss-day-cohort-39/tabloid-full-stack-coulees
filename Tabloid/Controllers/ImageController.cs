b42944d612f3e3a41243faa111940f5c1cea502a
Heidi Sprouse <hsprouse91@gmail.com>
2020-07-20 15:38:07 -0500

    large images are now resized before uploading

--- Tabloid/Controllers/ImageController.cs
+++ Tabloid/Controllers/ImageController.cs
@@ -38,13 +38,27 @@ namespace Tabloid.Controllers
             try
             {
                 using var image = Image.Load(file.OpenReadStream());
+
+                //shrink images that are too large
+                int originalImageWidth = image.Width;
+                int originalImageHeight = image.Height;
+
+                int maxWidth = 1110; //max width that we want a header image to be
+                if (originalImageWidth > maxWidth)
+                {
+                    //determine the proportional height of the resized image
+                    int newHeight = maxWidth * originalImageHeight;
+                    newHeight = newHeight / originalImageWidth;
+
+                    image.Mutate(x => x.Resize(maxWidth, newHeight));
+                }
+
                 image.Save(savePath + file.FileName);
             } catch
             {
                 return Conflict();
             }
 
-            //image.Mutate(x => x.Resize(256, 256));
             return Ok();
         }
 
